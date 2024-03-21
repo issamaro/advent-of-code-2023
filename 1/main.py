a3610b4cb8cdef0d469cd678f7af537c5b23d2c0
issamaro <aissa.c@outlook.fr>
2024-03-21 10:32:50 +0100

    added requests module to get the input from AOC

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,14 @@
+import requests
+
+SESSION_ID = "53616c7465645f5f32b8e7a7838ac83f734938c707a456e938e9dd4b9d981b30081f56c0c5e6d41e0451c4ca79401dea25c7192c55c6d79f9a06987e870bf9d8"
+AOC_URL = "https://adventofcode.com/2023/day/1/input"
+
+
 def main():
-    pass
+    input = requests.get(
+        AOC_URL, cookies={"session": SESSION_ID}
+    )
+    print(input.text)
     
     
 if __name__ == "__main__":
